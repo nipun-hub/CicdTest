0da1298d53d7e5c4fcba3e39c1572f68c658ad49
nipun-hub <nipunthemal@gmail.com>
2025-07-11 12:59:30 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { setupCounter } from './counter.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logoo" />
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
